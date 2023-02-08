600370238bce238cae2876b2edea59331dc96b86
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-02-08 21:29:38 +0100

    go

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+--  creates a table called users in the database
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` varchar(255) NOT NULL UNIQUE,
+    `name` varchar(255),
+    `country` enum('US', 'CO', 'TN') DEFAULT 'US'
+);
