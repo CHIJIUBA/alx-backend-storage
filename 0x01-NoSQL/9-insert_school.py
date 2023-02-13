a6b1120ed0fb260db1723fcdf1acaaa7e49169cf
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-02-13 23:48:37 +0100

    go

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""inserts a new document in a collection based on kwargs"""
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection based on kwargs"""
+    coll = {}
+    for key, value in kwargs.items():
+        coll[key] = value
+    mongo_collection.insert_one(coll)
